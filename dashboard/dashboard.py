18007f8921872ed71615b1b088fc51d68f02b54a
afifahrahma22 <134140580+afifahrahma22@users.noreply.github.com>
2024-04-06 16:46:39 +0700

    Update dashboard.py
    Revisi dashboard

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
+from io import BytesIO
 
 # membuat helper function
 def create_workingday_users_df(all_df):
@@ -53,13 +54,32 @@ max_date = all_df["dteday"].max()
 with st.sidebar:
     # Menambahkan logo perusahaan
     st.image("https://raw.githubusercontent.com/afifahrahma22/project-bike-sharing/main/image/Capital_BikeShare.png")
+    st.subheader("Selamat datang di Proyek Analisis Data - Capital Bikeshare")
 
-    # Mengambil start_date & end_date dari date_input
-    start_date, end_date = st.date_input(
-        label='Filter berdasarkan Tanggal', min_value=min_date,
-        max_value=max_date,
-        value=[min_date, max_date]
+    st.write(
+       """
+        \U0001F464 **Nama**: Dzakiyyah Afifah Rahma \n
+        :envelope: **Email**: punyakiyasaja@gmail.com \n
+        \U0001F194 **ID Dicoding**: afifahrahma \n
+       """ 
     )
+
+# melengkapi dashboard dengan visualisasi
+st.markdown(
+    """
+    # \U0001F6B4 Bike-Sharing Dashboard \U0001F6B4
+    """
+)
+
+col1, col2 = st.columns(2)
+
+with col1:
+# Mengambil start_date & end_date dari date_input
+    start_date, end_date = st.date_input(
+            label='Filter berdasarkan Tanggal', min_value=min_date,
+            max_value=max_date,
+            value=[min_date, max_date]
+    ) 
 
 # menyimpan data dari filter
 main_df = all_df[(all_df["dteday"] >= str(start_date)) & 
@@ -68,9 +88,6 @@ main_df = all_df[(all_df["dteday"] >= str(start_date)) &
 # memanggil helper function
 workingday_users_df = create_workingday_users_df(main_df)
 season_users_df = create_season_users_df(main_df)
-
-# melengkapi dashboard dengan visualisasi
-st.header("Bike-Sharing Dashboard \U0001F6B4")
 
 # informasi pengguna
 st.subheader('Informai Pengguna')
@@ -89,52 +106,90 @@ with col3:
     total_users = main_df['cnt'].sum()
     st.metric("Total Pengguna", value=total_users)
 
+# Color palette
+color_palette = sns.color_palette("flare")
+
+# visualisasi pengguna layanan bike sharing per hari
+st.markdown(
+    """
+    ##### Jumlah Pengguna Bike Sharing per Hari
+    """
+)
+
 # visualisasi pengguna layanan bike sharing per hari
 fig, ax = plt.subplots(figsize=(16,8))
 
-sns.lineplot(x="dteday", y="cnt", data=main_df, color="#72BCD4")
-ax.set_title("Jumlah Pengguna Bike Sharing per Hari", loc="center", fontsize=20)
-ax.set_xlabel("Tanggal", fontsize=15)
-ax.set_ylabel("Jumlah Pengguna", fontsize=15)
-ax.tick_params(axis='y', labelsize=12)
-ax.tick_params(axis='x', labelsize=12)
+sns.lineplot(x="dteday", y="cnt", data=main_df, color=color_palette[0])
+ax.set_ylabel("Jumlah Pengguna", fontsize=15, color="white")
+ax.tick_params(axis='y', labelsize=12, colors="white")
+ax.tick_params(axis='x', labelsize=12, colors="white")
+ax.set_facecolor('none') 
+plt.tight_layout()
 
-st.pyplot(fig)
+buffer = BytesIO()
+plt.savefig(buffer, format='png', transparent=True)
+buffer.seek(0)
+
+st.image(buffer)
 
 # visualisasi pengguna layanan bike sharing di hari kerja dan hari libur
+st.markdown(
+    """
+    ##### Jumlah Pengguna berdasarkan Hari Kerja dan Hari Libur (Tanpa Weekend)
+    """
+)
+
 fig, ax = plt.subplots(figsize=(16,8))
 
-sns.barplot(x="workingday", y="count", data=workingday_users_df, hue="categories")
-ax.set_title("Jumlah Pengguna berdasarkan Hari Kerja dan Hari Libur (Tanpa Weekend)", loc="center", fontsize=20)
-ax.set_xlabel("Kategori Hari", fontsize=15)
-ax.set_ylabel("Jumlah Pengguna", fontsize=15)
-ax.tick_params(axis='y', labelsize=12)
-ax.tick_params(axis='x', labelsize=12)
+sns.barplot(x="workingday", y="count", data=workingday_users_df, hue="categories", palette=color_palette)
+ax.set_ylabel("Jumlah Pengguna", fontsize=15, color="white")
+ax.tick_params(axis='y', labelsize=12, colors="white")
+ax.tick_params(axis='x', labelsize=12, colors="white")
+ax.set_facecolor('none') 
+plt.tight_layout()
 
-st.pyplot(fig)
+buffer = BytesIO()
+plt.savefig(buffer, format='png', transparent=True)
+buffer.seek(0)
+
+st.image(buffer)
 
 # visualisasi pengguna layanan bike sharing berdasarkan musim
-fig, ax = plt.subplots(figsize=(16,8))
+st.markdown(
+    """
+    ##### Jumlah Pengguna berdasarkan Musim
+    """
+)
+col1, col2 = st.columns(2)
 
-sns.barplot(x="season", y="count", data=season_users_df, color="#72BCD4")
-ax.set_title("Jumlah Pengguna Bike Sharing Berdasarkan Musim", loc="center", fontsize=20)
-ax.set_xlabel("Musim", fontsize=15)
-ax.set_ylabel("Jumlah Pengguna", fontsize=15)
-ax.tick_params(axis='y', labelsize=12)
-ax.tick_params(axis='x', labelsize=12)
+with col1:
+    fig, ax = plt.subplots()
+    sns.barplot(x="season", y="count", data=season_users_df, palette=color_palette)
+    ax.set_ylabel("Jumlah Pengguna", fontsize=15, color="white")
+    ax.tick_params(axis='y', labelsize=12, colors="white")
+    ax.tick_params(axis='x', labelsize=12, colors="white")
+    ax.set_facecolor('none') 
+    plt.tight_layout()
 
-st.pyplot(fig)
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png', transparent=True)
+    buffer.seek(0)
 
-# visualisasi kategori pengguna layanan bike sharing berdasarkan musim
-fig, ax = plt.subplots(figsize=(16,8))
+    st.image(buffer)
 
-sns.barplot(x="season", y="count", data=season_users_df, hue="categories")
-ax.set_title("Jumlah Pengguna Bike Sharing Berdasarkan Musim", loc="center", fontsize=20)
-ax.set_xlabel("Musim", fontsize=15)
-ax.set_ylabel("Jumlah Pengguna", fontsize=15)
-ax.tick_params(axis='y', labelsize=12)
-ax.tick_params(axis='x', labelsize=12)
+with col2:
+    fig, ax = plt.subplots()
+    sns.barplot(x="season", y="count", data=season_users_df, hue="categories", palette=color_palette)
+    ax.set_ylabel("Jumlah Pengguna", fontsize=15, color="white")
+    ax.tick_params(axis='y', labelsize=12, colors="white")
+    ax.tick_params(axis='x', labelsize=12, colors="white")
+    ax.set_facecolor('none') 
+    plt.tight_layout()
 
-st.pyplot(fig)
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png', transparent=True)
+    buffer.seek(0)
+
+    st.image(buffer)
 
 st.caption('Created by afifahrahma22')
